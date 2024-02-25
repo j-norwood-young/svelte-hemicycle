fd1afc658551ca4cfdde8c0eb5a8f7fa5f781b30
Jason Norwood-Young <jason@10layer.com>
2024-02-25 15:36:29 +0100

    Fix click

--- src/lib/Hemicycle.svelte
+++ src/lib/Hemicycle.svelte
@@ -61,6 +61,7 @@
             clicked = false;
         } else {
             current_party = point.data;
+            clicked = true;
         }
     }
 
@@ -99,8 +100,8 @@
                         on:mouseover={() => {selectParty(cell.site);}} 
                         on:focus={() => selectParty(cell.site)} on:mouseout={() => unselectParty()} 
                         on:blur={() => unselectParty()} role="button" tabindex={i}
-                        on:click={() => { clicked = !clicked; selectParty(cell.site)}}
-                        on:keypress={(e) => {if (e.key === "Enter") {clicked = !clicked; selectParty(cell.site);}}}
+                        on:click={() => { clickParty(cell.site)}}
+                        on:keypress={(e) => {if (e.key === "Enter") {clickParty(cell.site);}}}
                     />
                     {/if}
                 {/each}
@@ -112,6 +113,7 @@
         </g>
         <g transform={`translate(${r+padding/2}, ${r + (padding + 10)})`}>
             <!-- Input text data -->
+            <text x={0} y={0} text-anchor="middle" alignment-baseline="middle" fill={color} font-size={font_size}>{`${clicked}`}</text>
             {#if (current_party)}
             <text x={0} y={0} text-anchor="middle" alignment-baseline="middle" fill={color} font-size={font_size}>{`${current_party.Name} : ${current_party.Overall} seats; ${current_party.Regional} provincial; ${current_party.NationalPR} national`}</text>
             {/if}
