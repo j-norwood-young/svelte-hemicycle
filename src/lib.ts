3e9567daba1e04aed91cd028da925fa0dd605744
Jason Norwood-Young <jason@10layer.com>
2024-02-29 21:03:38 +0100

    Prep for publishing

---
+++ src/lib.ts
@@ -0,0 +1,2 @@
+import Hemicycle from "./lib/Hemicycle.svelte";
+export default Hemicycle;
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,14 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-  build: {
-    lib: {
-      entry: 'src/main.ts',
-      name: 'svelte-hemicycle',
-      fileName: (format) => `svelte-hemicycle.${format}.js`
-    }
-
-  }
-})
+    plugins: [svelte()],
+    build: {
+        lib: {
+            entry: "src/lib.ts",
+            name: "svelte-hemicycle",
+            fileName: (format) => `svelte-hemicycle.${format}.js`,
+        },
+    },
+});
